fae686059b1da78bfbc6a9fb4cd761e2fe9949d8
Coen Jacobs <coenjacobs@gmail.com>
2013-10-31 19:35:14 +0100

    Added git add alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,6 +10,7 @@ fi
 alias gbranch='git branch'
 alias gcommit='git commit'
 alias gdiff='git diff'
-alias gstatus='git status -sb' # show minimized list of status
+alias gstatus='git status -sb'
 alias gcheckout='git checkout'
 alias glog='git log --pretty=format:"%C(yellow)%h%Cred%d %Creset%s%Cblue [%cn]" --decorate' 
+alias gadd='git add'
