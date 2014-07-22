88a355ae0f98a407a658ab6a8c41aaa0047f25c7
Coen Jacobs <coenjacobs@gmail.com>
2014-07-22 09:35:01 +0200

    Add install file for symlinks

---
+++ scripts/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Setup symlinks for main dotfiles in home directory
+ln -s ~/.dotfiles/ack/.ackrc ~/.ackrc
+ln -s ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/vim/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
