7dd2071d5bb1bb98c6df7594c43b8b1add402d88
Coen Jacobs <coenjacobs@gmail.com>
2014-08-01 13:11:40 +0200

    Composer aliases

--- php/aliases.zsh
+++ php/aliases.zsh
@@ -1,2 +1,7 @@
 alias phpunit='vendor/bin/phpunit'
 alias artisan='php artisan'
+
+# Composer stuff
+alias cdump='composer dumpautoload'
+alias cinstall='composer install'
+alias cupdate='composer update'
