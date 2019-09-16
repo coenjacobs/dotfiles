35556d86d8760458dd36cfc82a3c9997025b85ae
Coen Jacobs <coenjacobs@gmail.com>
2019-09-16 21:01:56 +0200

    Added docker log feeding alias

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,8 +1,11 @@
 # docker container
 alias dls='docker container ls'
 
-#docker-compose
+# docker-compose
 alias dcrm='docker-compose rm'
 alias dcup='docker-compose up'
 alias dcdown='docker-compose down'
 alias dcbuild='docker-compose build'
+
+# docker logs
+alias dlog='docker logs -f $1'
